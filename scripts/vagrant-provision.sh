bb0fc6428b4f60a0c8fd692573ac19e82e2dd25b
Sam Lau <samlaudev@gmail.com>
2019-08-12 16:13:43 +0800

    install yarn and redis

--- scripts/vagrant-provision.sh
+++ scripts/vagrant-provision.sh
@@ -1,13 +1,22 @@
 #! /usr/bin/env bash
 
 apt-get update
-# nodejs
+# curl
 apt-get install -y curl
+
+# nodejs
 curl -sL https://deb.nodesource.com/setup_10.x | bash -
 apt-get install -y nodejs
+
+# yarn
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | tee /etc/apt/sources.list.d/yarn.list
+apt-get update && apt-get install -y yarn
+
 # mongodb community
 wget -qO - https://www.mongodb.org/static/pgp/server-4.0.asc | apt-key add -
 echo "deb http://mirrors.aliyun.com/mongodb/apt/ubuntu xenial/mongodb-org/4.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.0.list
-# echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.0 multiverse" | tee /etc/apt/sources.list.d/mongodb-org-4.0.list
-apt-get update
-apt-get install -y mongodb-org
+apt-get update && apt-get install -y mongodb-org
+
+# redis
+apt-get install -y redis-server
